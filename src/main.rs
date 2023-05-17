89a0b76bfe427d21d94ded63a0ec32036ded69ba
Michael van der Nest <michaelvdnest@gmail.com>
2023-05-17 18:11:20 +0200

    Trim input

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,18 @@ use std::io::stdin;
 
 fn main() {
     println!("Hello, what's your name?");
-    let mut name = String::new();
 
-    stdin()
-        .read_line(&mut name)             // Lend the name variable to the read_line function
-        .expect("Failed to read line");   // Expect the function work or terminate the program 
-    println!();
+    let name = what_is_your_name();
 
     println!("Hello, {name}");
+}
+
+fn what_is_your_name() -> String {
+    let mut name = String::new();
+    stdin()
+        .read_line(&mut name) // Lend the name variable to the read_line function
+        .expect("Failed to read line"); // Expect the function work or terminate the program
+    println!();
+
+    name.trim().to_string()
 }
